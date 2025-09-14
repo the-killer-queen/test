5dd66d533b83afc354a1596e4d9fdd96a23d853b
callmedeci <yunesmaghsoudie@gmail.com>
2025-09-14 11:58:16 +0330

    Add new shadcn components

---
+++ src/components/ui/switch.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import * as React from 'react';
+import * as SwitchPrimitives from '@radix-ui/react-switch';
+
+import { cn } from '../../lib/utils';
+
+export interface SwitchProps
+  extends React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root> {
+  checkedIcon?: React.ReactNode;
+  uncheckedIcon?: React.ReactNode;
+}
+
+const Switch = React.forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  SwitchProps
+>(
+  (
+    { className, checkedIcon, uncheckedIcon, onCheckedChange, ...props },
+    ref,
+  ) => {
+    const isControlled = props.checked !== undefined;
+
+    const [internalChecked, setInternalChecked] = React.useState(
+      props.defaultChecked ?? false,
+    );
+    const isChecked = isControlled ? props.checked : internalChecked;
+
+    const handleCheckedChange = (checked: boolean) => {
+      if (!isControlled) {
+        setInternalChecked(checked);
+      }
+      if (onCheckedChange) {
+        onCheckedChange(checked);
+      }
+    };
+
+    const effectiveCheckedIcon = checkedIcon;
+    const effectiveUncheckedIcon = uncheckedIcon || checkedIcon;
+    const icon = isChecked ? effectiveCheckedIcon : effectiveUncheckedIcon;
+
+    return (
+      <SwitchPrimitives.Root
+        className={cn(
+          'peer focus-visible:ring-ring focus-visible:ring-offset-background data-[state=checked]:bg-primary data-[state=unchecked]:bg-input inline-flex h-5 w-10 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+          className,
+        )}
+        onCheckedChange={handleCheckedChange}
+        {...props}
+        checked={isChecked}
+        ref={ref}
+      >
+        <SwitchPrimitives.Thumb
+          className={cn(
+            'bg-background pointer-events-none flex h-4 w-4 items-center justify-center rounded-full shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0',
+          )}
+        >
+          {icon && (
+            <span className='flex items-center justify-center'>{icon}</span>
+          )}
+        </SwitchPrimitives.Thumb>
+      </SwitchPrimitives.Root>
+    );
+  },
+);
+Switch.displayName = SwitchPrimitives.Root.displayName;
+
+export { Switch };
