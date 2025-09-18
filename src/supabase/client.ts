0f91e87f6050f6325397fefeb27229d8caf95e9b
callmedeci <yunesmaghsoudie@gmail.com>
2025-09-18 16:34:01 +0330

    use browser client

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,6 +1,5 @@
 import { Database } from '@/types/database';
 import { createBrowserClient } from '@supabase/ssr';
-import { createClient as createSupabaseClient } from '@supabase/supabase-js';
 
 export function createClient() {
   return createBrowserClient<Database>(
@@ -10,7 +9,7 @@ export function createClient() {
 }
 
 export function createBuildTimeClient() {
-  return createSupabaseClient<Database>(
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
