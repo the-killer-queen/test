909903a71d99f7176898947a0c9cc087d558714b
callmedeci <yunesmaghsoudie@gmail.com>
2025-09-23 15:04:33 +0330

    create pagination component

--- src/components/typography/P.tsx
+++ src/components/typography/P.tsx
@@ -1,8 +1,20 @@
+import { cn } from '@/lib/utils';
 import type { ReactNode } from 'react';
 
-export function P({ children }: { children: ReactNode }) {
+export function P({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
-    <p className='text-muted-foreground text-sm leading-relaxed text-pretty'>
+    <p
+      className={cn(
+        'text-muted-foreground text-sm leading-relaxed text-pretty',
+        className,
+      )}
+    >
       {children}
     </p>
   );
---
+++ src/components/ui/pagination.tsx
@@ -0,0 +1,201 @@
+'use client';
+
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  MoreHorizontalIcon,
+} from 'lucide-react';
+import * as React from 'react';
+
+import { Button, buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { parseAsInteger, useQueryState } from 'nuqs';
+import { RESULT_PER_PAGE } from '@/config/config';
+import { P } from '../typography/P';
+
+type PaginationContextProps = {
+  page: number;
+  totalPages: number;
+  handlePrev: () => void;
+  handleNext: () => void;
+};
+const PaginationContext = React.createContext<PaginationContextProps | null>(
+  null,
+);
+
+function Pagination({
+  className,
+  itemsLength,
+  ...props
+}: React.ComponentProps<'nav'> & { itemsLength: number }) {
+  const [page, updatePage] = useQueryState(
+    'page',
+    parseAsInteger.withDefault(1),
+  );
+  const totalPages = Math.ceil(itemsLength / RESULT_PER_PAGE);
+
+  function handleNext() {
+    if (page < totalPages) updatePage(page + 1);
+  }
+  function handlePrev() {
+    if (page > 1) updatePage(page - 1);
+  }
+
+  if (itemsLength === 0) return <P>No Items found!</P>;
+
+  return (
+    <PaginationContext.Provider
+      value={{ page, totalPages, handlePrev, handleNext }}
+    >
+      <div className='flex w-full items-center'>
+        <P
+          className={cn(
+            'w-full',
+            itemsLength <= RESULT_PER_PAGE && 'text-center',
+          )}
+        >
+          Showing <span>{(page - 1) * RESULT_PER_PAGE + 1}</span> to{' '}
+          <span>
+            {page === totalPages ? itemsLength : RESULT_PER_PAGE * page}
+          </span>{' '}
+          of <span>{itemsLength}</span> results
+        </P>
+
+        {itemsLength >= RESULT_PER_PAGE && (
+          <nav
+            role='navigation'
+            aria-label='pagination'
+            data-slot='pagination'
+            className={cn('mx-auto flex w-full justify-end', className)}
+            {...props}
+          />
+        )}
+      </div>
+    </PaginationContext.Provider>
+  );
+}
+
+function PaginationContent({
+  className,
+  ...props
+}: React.ComponentProps<'ul'>) {
+  return (
+    <ul
+      data-slot='pagination-content'
+      className={cn('flex flex-row items-center gap-1', className)}
+      {...props}
+    />
+  );
+}
+
+function PaginationItem({ ...props }: React.ComponentProps<'li'>) {
+  return <li data-slot='pagination-item' {...props} />;
+}
+
+type PaginationLinkProps = {
+  isActive?: boolean;
+} & Pick<React.ComponentProps<typeof Button>, 'size'> &
+  React.ComponentProps<typeof Button>;
+
+function PaginationLink({
+  className,
+  isActive,
+  size = 'icon',
+  ...props
+}: PaginationLinkProps) {
+  return (
+    <Button
+      disabled={!isActive}
+      variant={'ghost'}
+      aria-current={isActive ? 'page' : undefined}
+      data-slot='pagination-link'
+      data-active={isActive}
+      className={cn(
+        buttonVariants({
+          variant: isActive ? 'outline' : 'ghost',
+          size,
+        }),
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function PaginationPrevious({
+  className,
+  ...props
+}: React.ComponentProps<typeof PaginationLink>) {
+  const { page, handlePrev } = usePaginationContext();
+
+  return (
+    <PaginationLink
+      aria-label='Go to previous page'
+      size='default'
+      className={cn('gap-1 px-2.5 sm:pl-2.5', className)}
+      onClick={handlePrev}
+      isActive={page !== 1}
+      {...props}
+    >
+      <ChevronLeftIcon />
+      <span className='hidden sm:block'>Previous</span>
+    </PaginationLink>
+  );
+}
+
+function PaginationNext({
+  className,
+
+  ...props
+}: React.ComponentProps<typeof PaginationLink>) {
+  const { page, totalPages, handleNext } = usePaginationContext();
+
+  return (
+    <PaginationLink
+      aria-label='Go to next page'
+      size='default'
+      className={cn('gap-1 px-2.5 sm:pr-2.5', className)}
+      {...props}
+      onClick={handleNext}
+      isActive={page < totalPages}
+    >
+      <span className='hidden sm:block'>Next</span>
+      <ChevronRightIcon />
+    </PaginationLink>
+  );
+}
+
+function PaginationEllipsis({
+  className,
+  ...props
+}: React.ComponentProps<'span'>) {
+  return (
+    <span
+      aria-hidden
+      data-slot='pagination-ellipsis'
+      className={cn('flex size-9 items-center justify-center', className)}
+      {...props}
+    >
+      <MoreHorizontalIcon className='size-4' />
+      <span className='sr-only'>More pages</span>
+    </span>
+  );
+}
+
+function usePaginationContext() {
+  const context = React.use(PaginationContext);
+  if (!context)
+    throw new Error('usePaginationContext used outside its context');
+
+  return context;
+}
+
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+};
