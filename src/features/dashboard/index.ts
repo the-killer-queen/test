ccd32f793c39eb0c537cd98b4858536196eb7822
callmedeci <yunesmaghsoudie@gmail.com>
2025-09-26 15:23:14 +0330

    fix imports

--- src/features/dashboard/index.ts
+++ src/features/dashboard/index.ts
@@ -4,4 +4,4 @@ export { default as DashboardSidebar } from './components/DashboardSidebar';
 export { default as LogoutMenuItem } from './components/Logout';
 export { default as MenuNav } from './components/MenuNav';
 export { default as UserDropdown } from './components/UserDropdown';
-export { default as UserMenuNav, navLinks } from './components/UserMenuNav';
+export { default as UserMenuNav } from './components/UserMenuNav';
